676ef7a79ec1a0036d9b48834baa90d83839c343
akshay1 <akshays@linarc.io>
2022-01-22 12:17:12 +0530

    lec 3

---
+++ L3_Objects/content.js
@@ -0,0 +1,11 @@
+/**
+ * 4. Objects
+ *    Collection of name-value pair
+ *    {
+ *      1: "akshay",
+ *      2: 34,
+ *    }
+ *    we can have nested objects
+ *    each key schould be unique
+ *    you can give a funtion to the object
+ */
