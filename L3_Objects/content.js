79f06504772f24bfa2bb50e98249da3188ccdd13
akshay1 <akshays@linarc.io>
2022-01-22 12:17:43 +0530

    lec 3

--- L3_Objects/content.js
+++ L3_Objects/content.js
@@ -9,3 +9,49 @@
  *    each key schould be unique
  *    you can give a funtion to the object
  */
+
+// one way
+var myName = {1: "akshay"};
+console.log(myName);
+//another way
+var myObj = new Object();
+// litreal syntax
+var myOtherObj = {   
+    1: "dksjhgfsd",
+    2: "kdsjhfa",
+    3: "fljsdhf",
+    type: 'jdhgfs',
+    sayHi: function() {
+        console.log('hi');
+    },
+    sayType: function() {
+        console.log('hi' + this.type);
+    },
+};
+myOtherObj['key'] = "fkijsadhf";
+myOtherObj['key'] = [1,2,3];
+myObj['1'] = "akshay";
+myObj['2'] = "happy";
+console.log(myObj);
+console.log(myOtherObj);
+// access 
+
+console.log(myOtherObj['key']);
+console.log(myOtherObj.key);
+console.log(myOtherObj['1']);
+myOtherObj.sayHi();
+myOtherObj.sayType();
+
+
+//  Constructor pattern to create the object
+function DountConstructor(type, glazed, sweetness, hasChocolate) {
+    this.type = type;
+    this.glazed = glazed;
+    this.sweetness = sweetness;
+    this.hasChocolate = hasChocolate;
+    this.sayType2 = function() {
+        console.log('Type: ' + this.type);
+    }
+}
+var coconutDount = new DountConstructor("coconut", false, 8, true);
+console.log(coconutDount);
