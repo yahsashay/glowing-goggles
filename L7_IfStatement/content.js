160c2b745482fb1af1e1ffbcd2c0fc41b7a7e823
akshay1 <akshays@linarc.io>
2022-01-25 00:23:31 +0530

    lec 7

---
+++ L7_IfStatement/content.js
@@ -0,0 +1,14 @@
+/**
+ * If Statement
+ */
+
+// if (statement) {
+//     if statement is true do something we can write code that we want to run
+// }
+
+
+const shouddSayHi = true;
+
+if(shouddSayHi === true) {
+    console.log('Hi, There')
+}
