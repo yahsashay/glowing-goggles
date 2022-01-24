d0234fd18b76ae96eb71cf88770b9053b5ed017a
akshay1 <akshays@linarc.io>
2022-01-25 01:07:12 +0530

    lec 9

---
+++ L90_LogicalOperators/content.js
@@ -0,0 +1,13 @@
+/**
+ * Logical Operators
+ */
+
+// && logical And
+// || logical Or
+// !   logical Not
+
+let myName;
+console.log(myName);  // undefined
+
+console.log(!myName) // this will check null or undefined  so it evaluates to true
+console.log(myName === undefined || myName === null) // same as above statement
