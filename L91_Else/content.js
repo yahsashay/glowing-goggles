98bbfbc55bc7a0aecc6d220831ef2fbb4634690c
akshay1 <akshays@linarc.io>
2022-01-25 01:11:22 +0530

    lec 10

---
+++ L91_Else/content.js
@@ -0,0 +1,12 @@
+/**
+ * Else and ElseIf
+ * 
+ */
+const statement1 = 100 > 56;
+if(false) {
+    // do something
+} else if(statement1) {
+    // do something
+} else {
+    // then do something
+}
