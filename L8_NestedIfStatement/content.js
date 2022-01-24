bb0eaf68c5e9a7189639725b270cab490edea1e8
akshay1 <akshays@linarc.io>
2022-01-25 00:55:36 +0530

    lec 8

---
+++ L8_NestedIfStatement/content.js
@@ -0,0 +1,13 @@
+/**
+ * Nested If Statement
+ */
+
+if (true) {
+    if(true) {
+        // do something
+        // more if staement if you want
+    }
+    if(true) {
+        // dkjfgsikrfg
+    }
+}
